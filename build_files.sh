d5036420ebaaae73142ab8d2f05e1727a9d248f0
yash <yashjha7463@gmail.com>
2023-11-02 06:37:29 +0530

    hosting on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,18 @@
 
-python -m venv env
+# python -m venv env
+# source env/Scripts/activate
+# pip install psycopg2-binary
+# pip install -r requirements.txt 
+# python3.9 manage.py collectstatic
+
+project_dir="/config"
+
+# Create and activate a virtual environment
+python3 -m venv env
 source env/Scripts/activate
-pip install psycopg2-binary
-pip install -r requirements.txt 
-python3.9 manage.py collectstati
+
+# Navigate to your project directory
+cd "$project_dir"
+
+# Install your project's dependencies
+pip install -r requirements.txt
