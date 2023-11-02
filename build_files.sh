644f0736f828c0a81ccfbf9c894960a50f10f709
yash <yashjha7463@gmail.com>
2023-11-02 06:10:55 +0530

    hosting on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 build_files.sh
 
+source env/Scripts/activate
 pip install -r requirements.txt 
 python3.9 manage.py collectstatic
--- config/wsgi.py
+++ config/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'config.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
+# settings_module = 'config.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
